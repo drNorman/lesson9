b5433f428c64c803481ca251a3134debd0a358ca
drNorman <71461772+drNorman@users.noreply.github.com>
2024-09-09 22:19:16 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,4 +1,4 @@
-my_list = [42, 69, 322, 13, 0, 99, 5]
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 my_list_len = len(my_list)
 index = 0
 while index < my_list_len and my_list[index] >= 0:
